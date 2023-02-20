5865ea0505a7856b620c61ee00578d9536e7b1c1
Vincent Veysset <vincent.veysset.canada@gmail.com>
2023-02-20 16:07:34 +0100

    Init NextJS

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Playa - WebApp</title>
+      </Head>
+      
+      <h1>Playa - WebApp</h1>
+    </>
+  )
+};
