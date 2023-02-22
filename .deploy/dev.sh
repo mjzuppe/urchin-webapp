2cd975b6860b405318752ce760daa95058aa0034
Vincent Veysset <vincent.veysset.canada@gmail.com>
2023-02-22 12:10:29 +0100

    Add deploy script

---
+++ .deploy/dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Start DEV deployment"
+. ~/.nvm/nvm.sh
+cd ~/dev.playa.co
+git reset --hard
+git pull origin dev
+rm -rf node_modules
+rm -rf .next
+yarn install
+yarn build
+pm2 restart dev.playa.co
+echo "End DEV deployment"
