e9bd9073a393cec3b600e4fc4af0f41d755cd024
Alexandre Gros <xelaflash@gmail.com>
2023-02-27 13:34:05 -0500

    fix type

--- src/components/shared/customSelectSingle/SingleOption.tsx
+++ src/components/shared/customSelectSingle/SingleOption.tsx
@@ -1,22 +1,17 @@
 import classes from './CustomSelectSingle.module.scss';
 
-const getIconSrc = (icon) => {
-  return `/assets/includes-${icon}.svg`;
+interface SingleOptionProps {
+  info: any;
 }
 
-const SingleOption = ({
-  info,
-}) => {
+const SingleOption = ({ info }) => {
   return (
     <div className={classes.SingleOption}>
       <div className={classes.text_wrapper}>
         <p className={classes.text}>{info.label}</p>
       </div>
-      { info.icon && (
-        <img src={getIconSrc(info.icon)} />
-      )}
     </div>
-  )
-}
+  );
+};
 
 export default SingleOption;
