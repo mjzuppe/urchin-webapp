bcd1ec8c3aa8bc169caa411044c1e3628827792f
Alexandre Gros <xelaflash@gmail.com>
2023-03-02 11:34:05 -0500

    update types

--- src/types/Entries.ts
+++ src/types/Entries.ts
@@ -1,13 +1,14 @@
+import type { Taxonomies } from './Taxonomies';
+
 type Entry = {
   id: string;
   updatedAt: number;
   solanaAddress: string;
   arweaveAddress: string;
   title: string;
-  // metaDescription: string;
-  // hero image
-  // altText
-  // post (rich text)
+  taxonomies: Array<Taxonomies>;
+  template: string;
+  inputs: Array<string>;
 };
 
 type Entries = Array<Entry>;
