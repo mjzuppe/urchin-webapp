41fff156ff957d8d66d1d42d1c324ba1ba7a79ea
Alexandre Gros <xelaflash@gmail.com>
2023-03-06 10:25:09 -0500

    gtag

--- src/components/staticHomePage/index.tsx
+++ src/components/staticHomePage/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Script from 'next/script';
 
 // Styles
 import Image from 'next/image';
@@ -42,6 +43,23 @@ const StaticHomePage = () => {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-4K3HB47RXE`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4K3HB47RXE');
+          `,
+        }}
+      />
+
       <SEO />
       <div className={classes.background_wrapper}>
         <Image
