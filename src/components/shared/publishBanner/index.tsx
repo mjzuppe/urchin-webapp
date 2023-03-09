ec7224b50d10067d5d939d257e6fd452b73da14e
Alexandre Gros <xelaflash@gmail.com>
2023-03-09 10:27:20 -0500

    WIP
    - Start CreateTaxonomy
    - Start CreateTemplate
    - Dynamise changeLog

--- src/components/shared/publishBanner/index.tsx
+++ src/components/shared/publishBanner/index.tsx
@@ -5,11 +5,33 @@ import classes from './PublishBanner.module.scss';
 
 // Lib
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
+// Utils
+import { useAppSelector } from '../../../utils/useAppSelector';
 
 //  SDK
-// import { Keypair } from '@solana/web3.js';
-// import urchin from 'urchin';
-// import bs58 from 'bs58';
+import { Keypair } from '@solana/web3.js';
+import urchin from 'urchin';
+import bs58 from 'bs58';
+
+interface Taxonomy {
+  label: string;
+  parent?: string;
+}
+
+interface TemplateInputs {
+  label: string;
+  type: 'text' | 'textarea' | 'select' | 'numeric' | 'file';
+  options?: Array<string>;
+  validateInputs: boolean;
+  minLength?: number;
+  maxLength?: number;
+}
+
+interface Template {
+  title: string;
+  inputs: Array<TemplateInputs>;
+  taxonomies?: Array<Taxonomy>;
+}
 
 const PublishBanner = (): JSX.Element => {
   const [openChangeLog, setOpenChangeLog] = useState(false);
@@ -28,39 +50,107 @@ const PublishBanner = (): JSX.Element => {
     console.log('cancelHandler');
   };
 
-  // TODO: Dynamise when available
-  const changes = [
-    {
-      changeCategory: 'Template',
-      changeName: 'Ipsam voluptatem',
-    },
-    {
-      changeCategory: 'Taxonomy',
-      changeName: 'Lorem',
-    },
-  ];
+  // !! Taxonomies
+  const taxonomies = useAppSelector(
+    (state: any) => state.taxonomies.taxonomies
+  );
+  // console.log('taxonomies', taxonomies);
 
-  const preflightHandler = () => {
-    console.log('preflightHandler');
+  const taxonomiesToPublish = taxonomies.map((taxonomy: Taxonomy) => {
+    const { label, parent }: Taxonomy = taxonomy;
+    return {
+      label,
+      // parent, => should be a publickKey new PublicKey(string)
+    };
+  });
+  console.log('taxonomiesToPublish', taxonomiesToPublish);
+
+  // !! Templates
+  const templates = useAppSelector((state: any) => state.templates.templates);
+  // console.log('templates', templates);
+
+  const templatesToPublish = templates.map((template: Template) => {
+    const { title } = template;
+    const taxonomies = template?.taxonomies?.map((taxonomy: Taxonomy) => {
+      const { label }: Taxonomy = taxonomy;
+      return label;
+    });
+
+    const inputs = template.inputs.map((input: TemplateInputs) => {
+      const { label, type, options, validateInputs, minLength, maxLength } =
+        input;
+      return {
+        label,
+        type,
+        ...(type === 'select' && { options }),
+        ...(validateInputs && {
+          // validation: Joi.string().min(minLength).max(maxLength),
+          validation: {
+            minLength,
+            maxLength,
+          },
+        }),
+      };
+    });
+
+    return {
+      title,
+      taxonomies,
+      inputs,
+    };
+  });
+
+  // console.log('templatesToPublish', templatesToPublish);
+
+  const preflightHandler = async () => {
+    console.log('push to queue');
+    const payer = Keypair.fromSecretKey(
+      bs58.decode(
+        '4X6qkYZcGwu5KtLMLUXhf3F17born5or7sQwd3pfcFzuUkds5MPu3tUZXziboUzFVPqFJyqJXRsBzCEBY5exeQb5'
+      )
+    );
+
+    const connection = urchin({
+      payer,
+      cluster: 'devnet',
+    });
+
+    // console.log('connection', connection);
+
+    // Create taxonomy
+    // const createTaxonomy = connection.taxonomy.create(taxonomiesToPublish);
+    // console.log('createTaxonomy', createTaxonomy);
+
+    // Create template
+    // const createTemplate = connection.template.create(templatesToPublish);
+    // console.log('createTemplate', createTemplate);
+
+    // Preflight NOK yet
+    // const preflight = await connection.preflight();
+    // console.log('PREFLIGHT::', preflight);
   };
 
-  // const payer = Keypair.fromSecretKey(
-  //   bs58.decode(
-  //     '4X6qkYZcGwu5KtLMLUXhf3F17born5or7sQwd3pfcFzuUkds5MPu3tUZXziboUzFVPqFJyqJXRsBzCEBY5exeQb5'
-  //   )
-  // );
+  // TODO:  the same for entries
+  const taxonomiesChanges = {
+    changeCategory: 'Taxonomies',
+    //
+    changeName: taxonomiesToPublish
+      .map((taxonomy: any) => taxonomy.label)
+      .join(', '),
+  };
+  const templatesChanges = {
+    changeCategory: 'Templates',
+    changeName: templatesToPublish
+      .map((template: any) => template.title)
+      .join(', '),
+  };
 
-  // const connection = urchin({
-  //   payer,
-  //   cluster: 'devnet',
-  // });
-  // console.log('connection', connection);
-
-  // const test = connection.taxonomy.create({
-  //   label: 'testing',
-  //   owner: payer,
-  // });
-  // console.log('test', test);
+  const changes =
+    taxonomiesToPublish.length > 0 && templatesToPublish.length > 0
+      ? [taxonomiesChanges, templatesChanges]
+      : taxonomiesToPublish.length > 0
+      ? [taxonomiesChanges]
+      : [templatesChanges];
 
   return (
     <div className={classes.banner_container}>
