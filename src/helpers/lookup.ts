43a003347e6eaf785ae234bcff07779a3ba720c9
Vera Protopopova <vera.protopopova@gmail.com>
2023-03-15 16:28:53 -0400

    helper function

---
+++ src/helpers/lookup.ts
@@ -0,0 +1,38 @@
+
+// helper function for cases when we need to map all templates and entries at the same time
+export const mapTemplateAndEntryData = (entries: any, templates: any) => {
+  // templates.map(template => {
+  //   let templateEntries = entries.filter(entry => {
+  //     return entry.template == template.publicKey
+  //   })
+
+  //   let templateTextInput = template.inputs.filter(input => input.type === "text") ? template.inputs.filter(input => input.type === "text")[0] : null
+  //   let entriesInputs = templateEntries.map(entry => entry.inputs).flat()
+
+  //   let entryResult = entriesInputs.map(input => {
+  //     return {
+  //       ...input, 
+  //       name: input[templateTextInput.label]
+  //     }
+  //   })
+
+  //   return entryResult
+  // })
+}
+
+export const loadTemplateInputData = (entryInputs: any, templateInputs: any) => {
+  let templateTextInput = templateInputs.filter(input => input.type === "text")[0]
+
+  let entryWithName = {}
+
+  if(templateTextInput !== undefined) {
+    entryWithName = entryInputs.filter(input => {
+      return input[templateTextInput.label] !== null
+    })
+  }
+
+  return {
+    entryInputData: {...entryWithName}, 
+    templateInputData:{...templateTextInput}
+  }
+}
