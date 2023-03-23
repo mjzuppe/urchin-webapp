03f092bbce0aaec23b83e9d8ee753de6e093408d
Vincent Veysset <vincent.veysset.canada@gmail.com>
2023-03-23 10:10:24 +0100

    Block API call only for post

--- src/pages/api/send-transaction.tsx
+++ src/pages/api/send-transaction.tsx
@@ -1,5 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') return res.status(405).send(null);
+
+  // Data is in req.body
+  // Validation at this step
+
   res.status(200).json({ name: 'John Doe' })
 };
